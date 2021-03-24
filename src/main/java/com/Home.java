bbaf55bc3a7fdb9cb15dda7f4788418d25ae2a16
AnnieKatam <35347434+AnnieKatam@users.noreply.github.com>
2021-03-24 18:18:34 -0400

    adding condition to save info

--- src/main/java/com/Home.java
+++ src/main/java/com/Home.java
@@ -73,7 +73,7 @@ public class Home extends HttpServlet {
 			System.out.println("before create new user request dispatch");
 			RequestDispatcher dispatcher = getServletContext().getRequestDispatcher("/CaseReport.jsp");
 			dispatcher.forward(request, response);
-		}else if(userId!=null && saveUserInfo.equalsIgnoreCase("saveForm")) {
+		}else if(session.getAttribute("newUser")=="newUser" && saveUserInfo.equalsIgnoreCase("saveForm")) {
 			System.out.println("inside save form");
 			session.setAttribute("saveUser", "saveUser");
 			String dateStr = request.getParameter("date");
